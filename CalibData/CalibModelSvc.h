7612437d181c6efaf60c4669e4ce7b2f08237bde
jrb <>
2002-11-27 21:46:50 +0000

    1. Add serial number (of metadata row) field to CalibBase. 2. Put in accidentally omitted calibration type

--- CalibData/CalibModelSvc.h
+++ CalibData/CalibModelSvc.h
@@ -1,4 +1,4 @@
-// $Header:   $
+// $Header: /nfs/slac/g/glast/ground/cvs/CalibData/CalibData/CalibModelSvc.h,v 1.1 2002/11/26 18:20:29 jrb Exp $
 #ifndef CalibData_CalibModelSvc_h
 #define CalibData_CalibModelSvc_h
 
@@ -6,7 +6,7 @@
    @file CalibModelSvc.h
 
   A nearly-contentless class which only exists to make information
-  from CalibModel available to components other that CalibData.
+  from CalibModel available to components other than CalibData.
 
   For now just serve information needed by CalibDataSvc.
 */
