efb1d7faa1dda9bd4a596639751fa55651860533
jrb <>
2003-02-25 07:12:05 +0000

    Reimplement both putRange methods.

--- CalibData/Cal/CalCalibGain.h
+++ CalibData/Cal/CalCalibGain.h
@@ -1,4 +1,4 @@
-// $Header:  $
+// $Header: /nfs/slac/g/glast/ground/cvs/CalibData/CalibData/Cal/CalCalibGain.h,v 1.1 2003/02/25 06:19:50 jrb Exp $
 #ifndef CalibData_CalCalibGain_h
 #define CalibData_CalCalibGain_h
 
@@ -11,15 +11,20 @@ namespace CalibData {
   class CalCalibGain : public CalCalibBase {
 
   public:
-    CalCalibGain(unsigned nTower=16, unsigned nLayer=8, unsigned nXtal=12,
-                unsigned nFace=2, unsigned nRange=4) :
-      CalCalibBase(nTower, nLayer, nXtal, nFace, nRange) {}
+    CalCalibGain(unsigned nTowerRow=4, unsigned nTowerCol=4, 
+                 unsigned nLayer=8, 
+                 unsigned nXtal=12, unsigned nFace=2, unsigned nRange=4) :
+      CalCalibBase(nTowerRow, nTowerCol, nLayer, nXtal, nFace, nRange) {}
 
     ~CalCalibGain() {}
 
     /// Override putRange implementation in order to add consistency
     /// check
     bool putRange(idents::CalXtalId id, unsigned range, 
+                  unsigned face, RangeBase* data);
+
+    bool putRange(unsigned towerRow, unsigned towerCol, 
+                  unsigned layer, unsigned xtal, unsigned range,
                   unsigned face, RangeBase* data);
 
     virtual const CLID& clID() const {return classID(); }
