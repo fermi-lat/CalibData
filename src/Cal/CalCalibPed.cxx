d07b28baa1a6c82f6cb60052e4476347f074de78
jrb <>
2003-02-25 06:43:43 +0000

    CalCalibPed  -- re-implement both forms of putRange

--- src/Cal/CalCalibPed.cxx
+++ src/Cal/CalCalibPed.cxx
@@ -1,4 +1,4 @@
-// $Header:  $
+// $Header: /nfs/slac/g/glast/ground/cvs/CalibData/src/Cal/CalCalibPed.cxx,v 1.1 2003/02/25 06:22:01 jrb Exp $
 
 #include "CalibData/Cal/CalCalibPed.h"
 #include "CalibData/CalibModel.h"
@@ -13,6 +13,16 @@ namespace CalibData {
 
     // Otherwise go ahead and let base class handle it
     return CalCalibBase::putRange(id, range, face, data);
+  }
+
+  bool CalCalibPed::putRange(unsigned towerRow, unsigned towerCol, 
+                             unsigned layer, unsigned xtal, unsigned range,
+                             unsigned face, RangeBase* data) {
+    if (!dynamic_cast<Ped* >(data)) return false;
+
+    // Otherwise go ahead and let base class handle it
+    return CalCalibBase::putRange(towerRow, towerCol, layer, xtal,
+                                  range, face, data);
   }
 
 }
