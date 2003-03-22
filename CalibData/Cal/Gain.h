e28997b41a8aae4a23a412d36d2576a28d9294c0
jrb <>
2003-03-22 01:38:22 +0000

    Modifications to support new sigma field for gain and error for mu slope

--- CalibData/Cal/Gain.h
+++ CalibData/Cal/Gain.h
@@ -1,4 +1,4 @@
-// $Header: /nfs/slac/g/glast/ground/cvs/CalibData/CalibData/Cal/Gain.h,v 1.1 2003/02/25 06:19:50 jrb Exp $
+// $Header: /nfs/slac/g/glast/ground/cvs/CalibData/CalibData/Cal/Gain.h,v 1.2 2003/02/27 01:02:24 jrb Exp $
 #ifndef CalibData_Gain_h
 #define CalibData_Gain_h
 
@@ -8,15 +8,17 @@ namespace CalibData {
 
   class Gain : public RangeBase {
   public:
-    Gain(float gain = 0.0) : m_gain(gain) {}
+    Gain(float gain = 0.0, float sig = 0.0) : m_gain(gain), m_sig(sig) {}
     ~Gain() {}
 
     float getGain() {return m_gain;}
+    float getSig() {return m_sig;}
 
     virtual void update(RangeBase* other);
 
   private:
     float m_gain;
+    float m_sig;
 
   };
 }
--- CalibData/Cal/MuSlope.h
+++ CalibData/Cal/MuSlope.h
@@ -1,4 +1,4 @@
-// $Header: /nfs/slac/g/glast/ground/cvs/CalibData/CalibData/Cal/Gain.h,v 1.2 2003/02/27 01:02:24 jrb Exp $
+// $Header: /nfs/slac/g/glast/ground/cvs/CalibData/CalibData/Cal/MuSlope.h,v 1.1 2003/03/21 00:03:24 jrb Exp $
 #ifndef CalibData_MuSlope_h
 #define CalibData_MuSlope_h
 
@@ -8,15 +8,18 @@ namespace CalibData {
 
   class MuSlope : public RangeBase {
   public:
-    MuSlope(float slope = 0.0) : m_muSlope(slope) {}
+    MuSlope(float slope = 0.0, float error = 0.0) : m_muSlope(slope),
+    m_error(error) {}
     ~MuSlope() {}
 
     float getSlope() {return m_muSlope;}
+    float getError() {return m_error;}
 
     virtual void update(RangeBase* other);
 
   private:
     float m_muSlope;
+    float m_error;
 
   };
 }
