fb38e09cd7191407dadc762951c5c131a391a79b
jrb <>
2003-03-22 01:39:00 +0000

    Add new sigma field for Gain and error for MuSlope

--- src/Cal/MuSlope.cxx
+++ src/Cal/MuSlope.cxx
@@ -1,4 +1,4 @@
-// $Header: /nfs/slac/g/glast/ground/cvs/CalibData/src/Cal/Gain.cxx,v 1.1 2003/02/25 06:22:01 jrb Exp $
+// $Header: /nfs/slac/g/glast/ground/cvs/CalibData/src/Cal/MuSlope.cxx,v 1.1 2003/03/21 00:03:49 jrb Exp $
 #include "CalibData/Cal/MuSlope.h"
 
 namespace CalibData {
@@ -8,5 +8,6 @@ namespace CalibData {
     
     // check that otherMuSlope isn't 0 (dynamic cast worked)
     m_muSlope = otherMuSlope->m_muSlope;
+    m_error = otherMuSlope->m_error;
   }
 }
