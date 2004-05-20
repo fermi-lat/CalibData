bd4af4ff2e86d7909fabff09a33b511f8dba16bf
jrb <>
2004-05-20 23:54:34 +0000

    for CAL Light asymmetry calibration and integral nonlinearity (add them to list of calibrations to keep in calib. TDS)

--- src/CalibModel.cxx
+++ src/CalibModel.cxx
@@ -1,4 +1,4 @@
-// $Header: /nfs/slac/g/glast/ground/cvs/CalibData/src/CalibModel.cxx,v 1.12 2003/03/25 19:07:02 jrb Exp $
+// $Header: /nfs/slac/g/glast/ground/cvs/CalibData/src/CalibModel.cxx,v 1.13 2004/01/07 23:35:38 jrb Exp $
 
 #define _CalibData_CalibModel_cxx
 
@@ -109,6 +109,10 @@ public:
                                               CLID_Calib_CAL_MuSlope));
     CalibData::pairs.push_back(std::make_pair(CalibData::CAL_LightAtt,
                                               CLID_Calib_CAL_LightAtt));
+    CalibData::pairs.push_back(std::make_pair(CalibData::CAL_LightAsym,
+                                              CLID_Calib_CAL_LightAsym));
+    CalibData::pairs.push_back(std::make_pair(CalibData::CAL_IntNonlin,
+                                              CLID_Calib_CAL_IntNonlin));
     CalibData::pairs.push_back(std::make_pair(CalibData::Test_1,
                                               CLID_Calib_CalibTest1));
 
