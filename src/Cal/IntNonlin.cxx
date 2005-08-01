ddd489ea3a08f7b60e1066547c77b12e107c3ad5
jrb <>
2005-08-01 04:19:09 +0000

    Allow for per-channel "smoothed dacs" in IntNonlin calibrations

--- src/Cal/IntNonlin.cxx
+++ src/Cal/IntNonlin.cxx
@@ -1,12 +1,16 @@
-// $Header: /nfs/slac/g/glast/ground/cvs/CalibData/src/Cal/IntNonlin.cxx,v 1.1 2004/05/20 23:54:54 jrb Exp $
+// $Header: /nfs/slac/g/glast/ground/cvs/CalibData/src/Cal/IntNonlin.cxx,v 1.1 2004/05/26 22:36:45 jrb Exp $
 #include "CalibData/Cal/IntNonlin.h"
 
 namespace CalibData {
 
-  IntNonlin::IntNonlin(const std::vector<float>* values, float err) :
-      m_values(0), m_error(err) {
+  IntNonlin::IntNonlin(const std::vector<float>* values, float err, 
+                       const std::vector<float>* sdacs) :
+      m_values(0), m_sdacs(0), m_error(err) {
     if (values != 0) {
       m_values = new std::vector<float>(*values);
+    }
+    if (sdacs != 0) {
+      m_sdacs = new std::vector<float>(*sdacs);
     }
   }
 
@@ -15,6 +19,9 @@ namespace CalibData {
     
     // check that otherIntNonlin isn't 0 (dynamic cast worked)
     m_values = new std::vector<float>(*otherIntNonlin->m_values);
+    if (otherIntNonlin->m_sdacs) {
+      m_sdacs = new std::vector<float>(*otherIntNonlin->m_sdacs);
+    }
     m_error = otherIntNonlin->m_error;
   }
 }
