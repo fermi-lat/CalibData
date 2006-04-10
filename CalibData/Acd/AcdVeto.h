a6b6cc412429c87c08179a3caa46220a99cc4970
cvs2git <>
2006-04-10 05:44:34 +0000

    This commit was manufactured by cvs2svn to create branch 'EM-v6r070329p2'.
    Sprout from master 2005-08-01 23:20:23 UTC jrb 'AutoUpdate'
    Cherrypick from master 2006-04-10 05:44:33 UTC jrb 'Bug fixes in new Acd support':
        CalibData/Acd/AcdCalibCno.h
        CalibData/Acd/AcdCalibVeto.h
        CalibData/Acd/AcdCno.h
        CalibData/Acd/AcdVeto.h
        src/Acd/AcdCalibCno.cxx
        src/Acd/AcdCalibVeto.cxx
        src/Acd/AcdCno.cxx
        src/Acd/AcdFinder.h
        src/Acd/AcdVeto.cxx

---
+++ CalibData/Acd/AcdVeto.h
@@ -0,0 +1,28 @@
+// $Header: /nfs/slac/g/glast/ground/cvs/CalibData/CalibData/Acd/AcdVeto.h,v 1.1 2006/04/08 21:40:01 jrb Exp $
+#ifndef CalibData_AcdVeto_h
+#define CalibData_AcdVeto_h
+
+#include "CalibData/RangeBase.h"
+
+namespace CalibData {
+
+  class AcdVeto : public RangeBase {
+  public:
+    AcdVeto(float veto = 0.0, float width = 0.0, unsigned status=0) :
+      m_veto(veto), m_width(width), m_status(status) {}
+    ~AcdVeto() {}
+
+    float getVeto() const {return m_veto;}
+
+    float getWidth() const {return m_width;}
+    int getStatus() const {return m_status;}
+    virtual void update(RangeBase* other);
+
+  private:
+    float m_veto;
+    float m_width;
+    int m_status;
+  };
+}
+
+#endif
---
+++ src/Acd/AcdCalibVeto.cxx
@@ -0,0 +1,54 @@
+// $Header: /nfs/slac/g/glast/ground/cvs/CalibData/src/Acd/AcdCalibVeto.cxx,v 1.1 2006/04/08 21:40:01 jrb Exp $
+
+#include "CalibData/Acd/AcdCalibVeto.h"
+#include "AcdFinder.h"
+#include "CalibData/CalibModel.h"
+
+namespace CalibData {
+
+  AcdCalibVeto::AcdCalibVeto(unsigned nFace, unsigned nRow, unsigned nCol, 
+                           unsigned nNA, unsigned nPmt) :
+    AcdCalibBase(nFace, nRow, nCol, nNA, nPmt) {
+    unsigned ix = 0;
+    unsigned size = m_finder->getSize();
+
+    AcdVeto* pVetos = new AcdVeto[size];
+    for (ix = 0; ix < size; ix++) {
+      m_pmts[ix] = pVetos; 
+      ++pVetos;
+    }
+
+    // and similarly for NAs
+    size = m_finder->getNNASize();
+    AcdVeto* pNAs = new AcdVeto[size];
+    for (ix = 0; ix < size; ix++) {
+      m_NAs[ix] = pNAs; 
+      ++pNAs;
+    }
+
+  }
+
+  AcdCalibVeto::~AcdCalibVeto() {
+    AcdVeto* pVetos = dynamic_cast<AcdVeto* >(m_pmts[0]);
+    if (pVetos) delete [] pVetos;
+    pVetos = dynamic_cast<AcdVeto* >(m_NAs[0]);
+    if (pVetos) delete [] pVetos;
+  }
+
+  const CLID& AcdCalibVeto::classID()   {return CLID_Calib_ACD_ThreshVeto;}
+
+  bool AcdCalibVeto::putPmt(idents::AcdId id, unsigned pmt, RangeBase* data) {
+    if (!dynamic_cast<AcdVeto* >(data)) return false;
+
+    // Otherwise go ahead and let base class handle it
+    return AcdCalibBase::putPmt(id, pmt, data);
+  }
+  bool AcdCalibVeto::putPmt(unsigned face, unsigned row, unsigned col, 
+                              unsigned pmt, RangeBase* data) {
+    if (!dynamic_cast<AcdVeto* >(data)) return false;
+
+    // Otherwise go ahead and let base class handle it
+    return AcdCalibBase::putPmt(face, row, col, pmt, data);
+  }
+
+}
