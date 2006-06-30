a95311f3a4cca3c312d70a4b615bf8d6528ed12b
jrb <>
2006-06-30 19:19:26 +0000

    Store 'device' attribute

--- src/Anc/AncQdcPed.cxx
+++ src/Anc/AncQdcPed.cxx
@@ -1,4 +1,4 @@
-// $Header: /nfs/slac/g/glast/ground/cvs/CalibData/src/Acd/AcdPed.cxx,v 1.2 2006/04/08 21:40:01 jrb Exp $
+// $Header: /nfs/slac/g/glast/ground/cvs/CalibData/src/Anc/AncQdcPed.cxx,v 1.1 2006/06/29 19:50:42 jrb Exp $
 #include "CalibData/Anc/AncQdcPed.h"
 
 namespace CalibData {
@@ -10,5 +10,6 @@ namespace CalibData {
     m_val = otherPed->m_val;
     m_rms = otherPed->m_rms;
     m_isBad = otherPed->m_isBad;
+    m_device = otherPed->m_device;
   }
 }
