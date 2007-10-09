bf29637fcf7b251ae81af9fc5b89b021541f20b9
echarles <>
2007-10-09 18:15:25 +0000

    Added AcdHighRange, AcdRange, AcdCoherentNoise calibrations and reorganized acd stuff to avoid code duplication

---
+++ src/Acd/AcdCalibImpl.cxx
@@ -0,0 +1,36 @@
+// $Header: /nfs/slac/g/glast/ground/cvs/CalibData/src/Acd/AcdCalibBase.cxx,v 1.3 2006/04/10 05:44:33 jrb Exp $
+
+
+#include "CalibData/Acd/AcdPed.h"
+#include "CalibData/Acd/AcdGain.h"
+#include "CalibData/Acd/AcdVeto.h"
+#include "CalibData/Acd/AcdCno.h"
+#include "CalibData/Acd/AcdRange.h"
+#include "CalibData/Acd/AcdHighRange.h"
+#include "CalibData/Acd/AcdCoherentNoise.h"
+
+namespace CalibData {
+
+  int buildAcdCalibDescriptions() {
+    // careful, order matters within a calibration type
+
+    // pedestals
+    const AcdPedestalFitDesc& ped = AcdPedestalFitDesc::instance();
+    // gains (aka mip peaks)
+    const AcdGainFitDesc& gain = AcdGainFitDesc::instance();
+    // veto set points 
+    const AcdVetoFitDesc& veto = AcdVetoFitDesc::instance();
+    // cno set points
+    const AcdCnoFitDesc& cno = AcdCnoFitDesc::instance();
+    // range crossover
+    const AcdRangeFitDesc& range = AcdRangeFitDesc::instance();
+    // high range calibration
+    const AcdHighRangeFitDesc& highRange = AcdHighRangeFitDesc::instance();
+    // coherent noise 
+    const AcdCoherentNoiseFitDesc& coherentNois = AcdCoherentNoiseFitDesc::instance();    
+    return 0;
+  }
+    
+}
+
+int dummy = CalibData::buildAcdCalibDescriptions();
