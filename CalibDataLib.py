f6f73a9e0f3c05ad19ec7ae943eacfadde44eb52
jrb <>
2009-08-07 01:30:27 +0000

    Use registerTargets; add libraries appropriately .cvsignore:  add SCons build products

--- CalibDataLib.py
+++ CalibDataLib.py
@@ -1,8 +1,9 @@
-# $Header$
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/CalibData/CalibDataLib.py,v 1.1 2008/07/09 21:13:40 glastrm Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['CalibData'])
     env.Tool('identsLib')
+    env.Tool('facilitiesLib')
 
 def exists(env):
     return 1;
