dc084bb62b75287bfa1f23e1c8d48d3fddcc730d
jrb <>
2009-11-10 23:11:24 +0000

    Add gaudiLibs, clhepLibs

--- CalibDataLib.py
+++ CalibDataLib.py
@@ -1,9 +1,10 @@
-# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/CalibData/CalibDataLib.py,v 1.1 2008/07/09 21:13:40 glastrm Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/CalibData/CalibDataLib.py,v 1.2 2009/08/07 01:30:27 jrb Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['CalibData'])
+    env.Tool('addLibrary', library = env['gaudiLibs'])
+    env.Tool('addLibrary', library = env['clhepLibs'])
     env.Tool('identsLib')
     env.Tool('facilitiesLib')
-
 def exists(env):
     return 1;
